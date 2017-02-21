c7f4239652142a13dfdb4e95293dead202092356
Andrii Skrebets <skrebets.andrew@yandex.ru>
2017-02-21 14:51:48 +0200

    add bubble sort

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 (function () {
-    console.log('hello algorithms');
+
+    var arr = [2, 10, 5, 7, 11, 20, 3, 8];
+
+    // Bubble Sort
+    Array.prototype.bubbleSort = function () {
+        var arr = this,
+            tmp;
+
+        for(var i = 0; i < arr.length; i++) {
+            for(var j = 0; j < arr.length; j++) {
+                if(arr[i] < arr[j]) {
+                    tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+        }
+    };
+
+
 })();
